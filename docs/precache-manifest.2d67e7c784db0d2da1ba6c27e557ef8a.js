cc4ace7ce0d34fa4bb4923c6a331f4cf5a9a75f7
Jesus Fernandez <jesus.fdz93@gmail.com>
2020-12-14 15:03:33 -0600

    Segundo commit

---
+++ docs/precache-manifest.2d67e7c784db0d2da1ba6c27e557ef8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "115bd8d148281d6601eb7641f85820d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0b7d1c9ac044d3e31d7",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0b7d1c9ac044d3e31d7",
+    "url": "/static/js/main.36b3d8b2.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
